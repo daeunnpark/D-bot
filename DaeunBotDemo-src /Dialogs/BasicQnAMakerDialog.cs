58cc1f9718b7a384750ae6c93266436f8e74a7f1
daeunnpark <daeunn@gmail.com>
2018-08-10 12:15:37 +0900

    Herocard(image card) added

--- DaeunBotDemo-src /Dialogs/BasicQnAMakerDialog.cs
+++ DaeunBotDemo-src /Dialogs/BasicQnAMakerDialog.cs
@@ -21,6 +21,95 @@ namespace Microsoft.Bot.Sample.QnABot
 
 
         public RootDialog() : base(new QnAMakerService(new QnAMakerAttribute(RootDialog.GetSetting("QnAAuthKey"),Utils.GetAppSetting("QnAKnowledgebaseId"),"this - No good match in FAQ.", 0.5)))
+        {
+
+        }
+        
+        protected override async Task RespondFromQnAMakerResultAsync(IDialogContext context, IMessageActivity message, QnAMakerResults result)
+        { 
+            var answer = result.Answers.First().Answer;
+            Activity reply = ((Activity)context.Activity).CreateReply();
+
+            string[] qnaAnswerData = answer.Split(';');
+            int dataSize = qnaAnswerData.Length;
+
+            // image or video
+            if (dataSize>1 && dataSize<=6){
+
+            var attachment = GetSelectedCard(answer);
+            reply.Attachments.Add(attachment);
+            await context.PostAsync(reply);
+
+            } else {
+
+            await context.Forward(new BasicQnAMakerDialog(), AfterAnswerAsync, message, CancellationToken.None);
+            await context.PostAsync(reply);
+
+            }
+        }
+
+        private static Attachment GetSelectedCard(string answer){
+
+        int len = answer.Split(';').Length;
+            
+            switch (len){
+            
+            case 4: return GetHeroCard(answer);
+            //case 6: return GetVideoCard(answer); 
+            default: return GetHeroCard(answer);
+            
+            }
+
+        }
+
+        private static Attachment GetHeroCard(string answer){
+            string[] qnaAnswerData = answer.Split(';');
+            string title = qnaAnswerData[0];
+            string description= qnaAnswerData[1];
+            string url = qnaAnswerData[2];
+            string imageURL= qnaAnswerData[3];
+
+            HeroCard card = new HeroCard{
+            Title = title,
+            Subtitle = description,
+            };
+
+            card.Buttons = new List<CardAction>{
+                new CardAction(ActionTypes.OpenUrl,"Learn More", value: url)
+            };
+
+            card.Images = new List<CardImage>{
+            new CardImage (url = imageURL)
+            };
+
+            retrun card.ToAttachement();
+        }
+
+
+/* From 10min
+         private static Attachment GetVideoCard(string answer){
+            string[] qnaAnswerData = answer.Split(';');
+            string title = qnaAnswerData[0];
+             string subtitle= qnaAnswerData[1];
+            string url = qnaAnswerData[2];
+            string imageURL= qnaAnswerData[3];
+
+            HeroCard card = new HeroCard{
+            Title = title,
+            Subtitle = description,
+            };
+
+            card.Buttons = new List<CardAction>{
+                new CardAction(ActionTypes.OpenUrl,"Learn More", value: url)
+            };
+
+            card.Images = new List<CardImage>{
+            new CardImage (url = imageURL)
+            };
+
+            retrun card.ToAttachement();
+        }
+*/
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
